0f624962f40582e177d1efc31ed967e70757fd29
Mhizzpwresh <Mhizzpwresh@gmail.com>
2022-09-08 09:35:24 -0700

    A SCRIPT THAT USES PRINTF

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - This prints the phrase with proper grammar
+*Return: 0 you should alway add your return when coding in c
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
