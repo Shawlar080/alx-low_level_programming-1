6779c137ed0db705e1bb4d8a73b95f59b35fc423
Mhizzpwresh <Mhizzpwresh@gmail.com>
2022-09-15 15:08:56 -0700

    My task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,16 +6,12 @@
 * Return: Always 0
 */
 
-void main(void)
+int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i == 100)
-{
-printf("Buzz ");
-}
-else if (i % 3 && i % 5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
 }
