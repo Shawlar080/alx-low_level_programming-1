32b831629d00757a30be49ce140a126d7af88f34
Mhizzpwresh <Mhizzpwresh@gmail.com>
2022-09-15 13:44:44 -0700

    My task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* main - a program that prints the numbers from 1 to 100
+* Return: Always 0
+*/
+
+int main(int)
+{
+int i;
+for (i = 1; i < 101; i++)
+{
+if (i % 3 = 0)
+{
+printf("%d ", Fizz);
+}
+else if (i % 5 = 0)
+{
+printf("%d ", Buzz);
+}
+else if (i % 3 = 0 && i % 5 = 0)
+{
+printf("%d ", FizzBuzz);
+}
+else
+{
+printf("%d ", i);
+}
+printf('\n');
+}
+}
