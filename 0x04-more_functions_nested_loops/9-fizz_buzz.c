ea5149df12d65020914032d1a39d71dd23ff8580
Mhizzpwresh <Mhizzpwresh@gmail.com>
2022-09-15 14:02:26 -0700

    My task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
 * main - a program that prints the numbers from 1 to 100
@@ -12,20 +12,25 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
 {
-printf(" Fizz");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf(" Buzz");
+printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf(" FizzBuzz");
+printf("FizzBuzz");
 }
 else
 {
-printf(" %d", i);
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
 }
 printf('\n');
+return (0);
 }
 }
